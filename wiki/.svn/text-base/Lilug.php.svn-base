c93729f2903a8c511f63fbfd0786f3cbcdf6718d
mdrago <>
2006-04-02 05:36:36 -0400

    [svn] import the lilug wiki stuff
    --- hg-to-git merged commit ---
    
    [svn] Make a lilug directory for all things Lilug

---
+++ wiki/.svn/text-base/Lilug.php.svn-base
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Lilug
+ *
+ * This skin was adapted from the monobook theme.  A lot of the stuff in here
+ * is just left over from that skin.  It wasn't removed in order to avoid
+ * disturbing things too much.  That's all.
+ *
+ * February 2006: Adapted from Monobook by Mark Drago
+ *
+ * @package Lilug
+ * @subpackage Skins
+ */
+
+if( !defined( 'MEDIAWIKI' ) )
+	die();
+
+/** */
+require_once('includes/SkinTemplate.php');
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+class SkinLilug extends SkinTemplate {
+	/** Using lilug. */
+	function initPage( &$out ) {
+		SkinTemplate::initPage( $out );
+		$this->skinname  = 'lilug';
+		$this->stylename = 'lilug';
+		$this->template  = 'LilugTemplate';
+	}
+}
+
+/**
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+class LilugTemplate extends QuickTemplate {
+	/**
+	 * Template filter callback for Lilug skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
+		// Suppress warnings to prevent notices about missing indexes in $this->data
+		wfSuppressWarnings();
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
+  <head>
+    <meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
+    <?php $this->html('headlinks') ?>
+    <title><?php $this->text('pagetitle') ?></title>
+    <style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css"; /*]]>*/</style>
+    <link rel="stylesheet" type="text/css" <?php if(empty($this->data['printable']) ) { ?>media="print"<?php } ?> href="<?php $this->text('stylepath') ?>/common/commonPrint.css" />
+    <!--[if lt IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE50Fixes.css";</style><![endif]-->
+    <!--[if IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE55Fixes.css";</style><![endif]-->
+    <!--[if gte IE 6]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE60Fixes.css";</style><![endif]-->
+    <!--[if IE]><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js"></script>
+    <meta http-equiv="imagetoolbar" content="no" /><![endif]-->
+    <?php if($this->data['jsvarurl']) { ?><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl') ?>"></script><?php } ?>
+    <script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/wikibits.js"></script>
+    <?php if($this->data['usercss']) { ?><style type="text/css"><?php $this->html('usercss') ?></style><?php } ?>
+    <?php if($this->data['userjs']) { ?><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('userjs') ?>"></script><?php } ?>
+    <?php if($this->data['userjsprev']) { ?><script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script><?php   } ?>
+    <?php if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
+  </head>
+  <body <?php if($this->data['body_ondblclick']) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+        <?php if($this->data['body_onload'    ]) { ?>onload="<?php     $this->text('body_onload')     ?>"<?php } ?>
+        <?php if($this->data['nsclass'        ]) { ?>class="<?php      $this->text('nsclass')         ?>"<?php } ?>>
+    <div id="header"><a href="http://localhost/wiki/index.php/Main_Page"><img src="/wiki2/skins/lilug/lilug_logo.png" /></a></div>    
+    <div id="globalWrapper">
+      <div id="column-content">
+        <table id="topnav"><tr>
+        <td><a href="/wiki/index.php/Main_Page">Home</a></td>
+        <td><a href="/wiki/index.php/About">About</a></td>
+        <td><a href="/wiki/index.php/Meetings">Meetings</a></td>
+        <td><a href="/wiki/index.php/Directions">Directions</a></td>
+        <td><a href="/wiki/index.php/Mailing_Lists">Mailing Lists</a></td>
+        </tr></table>
+	<div id="content">
+	  <a name="top" id="top"></a>
+	  <?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+	  <h1 class="firstHeading"><?php $this->text('title') ?></h1>
+	  <div id="bodyContent">
+	    <h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+	    <div id="contentSub"><?php $this->html('subtitle') ?></div>
+	    <?php if($this->data['undelete']) { ?><div id="contentSub"><?php     $this->html('undelete') ?></div><?php } ?>
+	    <?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+	    <!-- start content -->
+	    <?php $this->html('bodytext') ?>
+	    <?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
+	    <!-- end content -->
+	    <div class="visualClear"></div>
+	  </div>
+	</div>
+      <table id="meta"><tr id="toprow"><td>
+      <div class="wiki_links">
+        <?php
+        foreach($this->data['content_actions'] as $key => $action) {
+	    echo "<a href=\"";
+	    echo htmlspecialchars($action['href']) . "\">";
+	    echo htmlspecialchars($action['text']) . "</a><br />\n";
+	}
+	?>
+      </div>
+      </td><td>
+      <div class="wiki_links">
+        <?php
+	foreach($this->data['personal_urls'] as $key => $item) {
+	    echo "<a href=\"" . htmlspecialchars($item['href']) . "\"";
+	    if(!empty($item['class'])) {
+	      echo "class=\"" . htmlspecialchars($item['class']) . "\"";
+	    }
+	    echo ">";
+	    echo htmlspecialchars($item['text']);
+	    echo "</a><br />\n";
+	}
+	?>
+      </div>
+      </td><td>
+    <div class="wiki_links">
+    <?php
+    if ($this->data['notspecialpage']) {
+        foreach(array('whatlinkshere', 'recentchangeslinked') as $special ) {
+	  echo "<a href=\"" . htmlspecialchars($this->data['nav_urls'][$special]['href']) . "\">";
+	  echo $this->msg($special) . "</a><br />\n";
+	}
+    }
+
+    if (isset($this->data['nav_urls']['trackbacklink'])) {
+	echo "<a href=\"" . htmlspecialchars($this->data['nav_urls']['trackbacklink']['href']) . "\">";
+	echo $this->msg('trackbacklink') . "</a><br />\n";
+    }
+
+    if ($this->data['feeds']) {
+	foreach($this->data['feeds'] as $key => $feed) {
+	    echo "<a href=\"" . htmlspecialchars($feed['href']) . "\">";
+	    echo htmlspecialchars($feed['text']) . "</a><br />\n";
+	}
+    }
+    
+    foreach(array('contributions', 'emailuser', 'upload', 'specialpages') as $special ) {
+	if($this->data['nav_urls'][$special]) {
+	    echo "<a href=\"" . htmlspecialchars($this->data['nav_urls'][$special]['href']) . "\">";
+	    echo $this->msg($special) . "</a><br />\n";
+	}
+    }
+    
+    if(!empty($this->data['nav_urls']['print']['href'])) {
+	echo "<a href=\"" . htmlspecialchars($this->data['nav_urls']['print']['href']) . "\">";
+	echo $this->msg('printableversion') . "</a><br />\n";
+    }
+    ?>
+    </div>
+    </td></tr><tr><td colspan="3">
+    <div id="search">
+      <form name="searchform" action="<?php $this->text('searchaction') ?>" id="searchform">
+      <?php
+      if( isset( $this->data['search'] ) ) {
+	$searchval = "value=\"" . $this->text('search') . "\" ";
+      }
+      ?>
+        <input id="searchInput" name="search" type="text" <?=$searchval?>/>
+        <input type='submit' name="go" class="searchButton" id="searchGoButton" value="Go" />&nbsp;<input type='submit' name="fulltext" class="searchButton" value="Search" />
+      </form>
+    </div>
+    </td></tr></table>
+    </div>
+    <?php $this->html('reporttime') ?>
+  </body>
+</html>
+<?php
+	wfRestoreWarnings();
+	}
+}
+?>
---
+++ wiki/lilug_ext.php
@@ -0,0 +1,359 @@
+<?php
+
+$wgExtensionFunctions[] = "register_lilug";
+
+#register the extension with mediawiki
+function register_lilug() {
+    global $wgParser;
+
+    #register the function which will handle these hooks
+    $wgParser->setHook("meetingdatetime", "render_meetingdatetime");
+    $wgParser->setHook("nextmeetingdesc", "render_nextmeetingdesc");
+}
+
+#function that is called by mediawiki when someone uses the
+#<meetingdatetime> tag.  It takes the input from the tag and
+#returns the text that should replace the tag.
+function render_meetingdatetime($input, $argv) {
+    global $wgTitle;
+
+    #disable cache for pages using this tag
+    wfPurgeSquidServers(array($wgTitle->getInternalURL()));
+    $wgTitle->invalidateCache();
+
+    #the 'group' attribute is required
+    if (!isset($argv["group"])) {
+	return "ERROR: A 'group' attribute is needed with 'meetingdatetime'";
+    }
+
+    #zeros mean use the current month & year
+    $month = 0;
+    $year = 0;
+
+    if (isset($argv["month"])) {
+	$month = $argv["month"];
+    }
+
+    if (isset($argv["year"])) {
+	$year = $argv["year"];
+    }
+
+    $meeting_time = get_groups_meeting($argv["group"], $month, $year);
+
+    return get_nice_text_for_time($meeting_time);
+
+}
+
+function render_nextmeetingdesc($input, $argv) {
+    global $wgOut;
+    global $wgTitle;
+
+    #disable cache for pages using this tag
+    wfPurgeSquidServers(array($wgTitle->getInternalURL()));
+    $wgTitle->invalidateCache();
+
+    #the 'group' attribute is required
+    if (!isset($argv["group"])) {
+	return "ERROR: A 'group' attribute is needed with 'meetingdatetime'";
+    }
+
+    #decide if we're making a link or if we're including a page
+    $begin_tags = "";
+    $end_tags = "";
+
+    $is_link = false;
+    if ($argv["type"] == "link") {
+	$is_link = true;
+	$begin_tags = "[[";
+	$end_tags = "]]";
+    } elseif ($argv["type"] == "include") {
+	$begin_tags = "{{";
+	$end_tags = "}}";
+    }
+
+    #figure out the month and year of the meeting
+    $meeting_time = get_groups_meeting($argv["group"], 0, 0);
+    $month = date("m", $meeting_time);
+    $year = date("Y", $meeting_time);
+
+    $group = ucfirst($argv["group"]);
+
+    $string = $begin_tags . "Template:$group Meeting $year $month";
+    
+    #potentially add link title
+    if (is_link == true) {
+	if ((isset($input)) and ($input != "")) {
+	    $string .= "|$input";
+	} else {
+	    $string .= "|$group Meeting $year $month";
+	}
+    }
+
+    $string .= $end_tags;
+
+    return $wgOut->parse($string, false);
+}
+
+#Get the meeting time for different groups depending on when their meetings
+#are held.  This returns a unix timestamp representing the time that the
+#meeting starts.
+function get_groups_meeting($group, $month, $year) {
+
+    switch ($group) {
+    case "lilugirc":
+	if (($month != 0) or ($year != 0)) {
+	    print "ERROR: 'lilugirc' will only get the next meeting time";
+	    return 0;
+	}
+	$meeting_time = lilug_irc_meeting_time();
+	break;
+    case "lilugsisig":
+	$meeting_time = monthly_meeting_time(-1, 4, 20, 0, $month, $year);
+	break;
+    case "lilugdevsig":
+	$meeting_time = monthly_meeting_time(3, 4, 20, 0, $month, $year);
+	break;
+    default:
+    case "lilug":
+	$meeting_time = monthly_meeting_time(2, 2, 20, 0, $month, $year);
+	break;
+    }
+
+    return $meeting_time;
+}
+
+#A special function which determines when the next irc meeting is.  It is
+#special because irc meetings follow a special schedule in that they're every
+#Tuesday night, except for nights when we have a regular meeting.  It currently
+#only supports getting the datetime of the very next meeting.  It returns that
+#time as a unix timestamp.
+function lilug_irc_meeting_time() {
+    #general constants used
+    $length_of_week = 60 * 60 * 24 * 7;
+
+    #constants that define the meeting time
+    $day_of_meeting = 2;     # tuesdays
+    $hour_of_meeting = 20;   # 8pm
+    $minutes_of_meeting = 0; # 8:_00_ pm
+
+    $meeting_time = next_weekly_meeting_time(time(),
+					     $day_of_meeting,
+					     $hour_of_meeting,
+					     $minutes_of_meeting);
+
+    #check for conflict with regular meeting
+    $meeting_date = idate("d", $meeting_time);
+    $meeting_month = idate("m", $meeting_time);
+    $meeting_year = idate("Y", $meeting_time);
+
+    #constants that define the day of conflict (regular lilug meetings)
+    $reg_week_of_meeting = 2;    #_second_ tuesday
+    $reg_day_of_meeting = 2;     # tuesday
+    $reg_hour_of_meeting = 20;   # 8pm
+    $reg_minutes_of_meeting = 0; # 8:_00_ pm
+
+    $regular_meeting = monthly_meeting_time($reg_week_of_meeting,
+					    $reg_day_of_meeting,
+					    $reg_hour_of_meeting,
+					    $reg_minutes_of_meeting);
+
+    $reg_meeting_date = idate("d", $regular_meeting);
+    $reg_meeting_month = idate("m", $regular_meeting);
+    $reg_meeting_year = idate("Y", $regular_meeting);
+
+    if (($meeting_date == $reg_meeting_date) and
+	($meeting_month == $reg_meeting_month) and
+	($meeting_year == $reg_meeting_year)) {
+      #skip to the next week in this case
+      $meeting_time = next_weekly_meeting_time(time() + $length_of_week,
+					       $day_of_meeting,
+					       $hour_of_meeting,
+					       $minutes_of_meeting);
+
+    }
+
+    return $meeting_time;
+}
+
+#This function figures out the datetime of a monthly meeting given the
+#number of the week, the day of the week, the hours and the minutes that a
+#meeting is held.  It will get the time of the meeting in the current month
+#if the month and year parameters are zeros.  Otherwise, it will get the
+#time of the meeting using the given month and year.  This function can also
+#calculate the time of a meeting whose schedule is relative to the end of
+#the month (ie the _last_ thursday of the month) if a negative number is passed
+#in for the number of the week.  It's pretty sweet.
+function time_of_monthly_meeting($week_of_meeting, $day_of_meeting,
+				 $hour_of_meeting, $minutes_of_meeting,
+				 $month = 0, $year = 0) {
+    global $wgOut;
+
+    #use current month if zero is passed in
+    if ($month == 0) {
+	$month = idate("m");
+    }
+
+    #use current year if zero is passed in
+    if ($year == 0) {
+	$year = idate("Y");
+    }
+
+    #figure out which direction we'll be moving in
+    if ($week_of_meeting > 0) {
+	$direction = 1;
+    } else {
+	$direction = -1;
+    }
+
+    #get the starting day (first or last of month)
+    if ($direction > 0) {
+	$starting_date = 1;
+    } else {
+        #to figure out the number of days in the month, we need a datetime
+        #that exists in that month, so we use the first of the month
+	$first_of_month = mktime(0, 0, 0, $month, 1, $year);
+	$starting_date = idate("t", $first_of_month);
+    }
+
+    #get the unix time of the day we'll be starting from
+    $starting_day = mktime(0, 0, 0, $month, $starting_date, $year);
+
+    #get the day of the week of the starting_day
+    $day_of_starting_day = idate("w", $starting_day);
+
+    #find the distance to the meeting day_of_the_week
+    if ($day_of_starting_day == $day_of_meeting) {
+	$days_until_next_day = 0;
+    } elseif (($direction * $day_of_starting_day) < 
+	      ($direction * $day_of_meeting)) {
+	$days_until_next_day = $day_of_meeting - $day_of_starting_day;
+    } else {
+	$days_until_next_day = ((7 * $direction) -
+				($day_of_starting_day - $day_of_meeting));
+    }
+
+    #move to the correct week
+    $date_of_meeting = ((($week_of_meeting - $direction) * 7) +
+			($starting_date + $days_until_next_day));
+
+    #exact time of next meeting
+    $exact_time = mktime($hour_of_meeting, $minutes_of_meeting, 0, $month,
+			 $date_of_meeting, $year);
+
+    return $exact_time;
+}
+
+#This function wraps time_of_monthly_meeting and handles the instance that
+#this month's meeting has already passed.  When this happens this function
+#handles figuring out the time of the meeting next month.  So, if a meeting
+#was scheduled for May 16th and it is May 20th, this function is the one
+#that decides that we should return the time for the meeting in June.
+function monthly_meeting_time($week_of_meeting, $day_of_meeting,
+			      $hour_of_meeting, $minutes_of_meeting, $month=0,
+			      $year=0)
+{
+    #estimated constant length of meetings
+    $meeting_length = 60 * 60 * 4;  # 4 hours
+
+    #test to see if we want to know when the next meeting is
+    $next_meeting = true;
+    if (($month != 0) or ($year != 0)) {
+	$next_meeting = false;
+    }
+	
+    $meeting_time = time_of_monthly_meeting($week_of_meeting, $day_of_meeting,
+					    $hour_of_meeting,
+					    $minutes_of_meeting, $month,
+					    $year);
+
+    #if we're not trying to figure out the very next meeting we
+    #just return the time we got already
+    if ($next_meeting == false) {
+	return $meeting_time;
+    }
+
+    #check to see if this meeting time has passed already
+    if ($meeting_time < (time() - $meeting_length)) {
+	$meeting_month = idate("m", $meeting_time);
+
+	if ($meeting_month == 12) {
+	    $month = 1;
+	    $year++;
+	} else {
+	    $month = $meeting_month + 1;
+	}
+
+	$meeting_time = time_of_monthly_meeting($week_of_meeting,
+						$day_of_meeting,
+						$hour_of_meeting,
+						$minutes_of_meeting, $month,
+						$year);
+    }
+
+    return $meeting_time;
+}
+
+#This function returns the datetime of a meeting that is held weekly.  It only
+#handles figuring out the next weekly meeting.  It returns the datetime as a
+#unix timestamp.
+function next_weekly_meeting_time($starting_time, $day_of_meeting,
+				  $hour_of_meeting, $minutes_of_meeting)
+{
+    #estimated constant length of meetings
+    $length_of_day = 60 * 60 * 24;
+
+    $current_day = idate("w", $starting_time);
+    $current_date = idate("d", $starting_time);
+    $current_month = idate("m", $starting_time);
+    $current_year = idate("Y", $starting_time);
+
+    if ($current_day == $day_of_meeting) {
+	$days_until_meeting = 0;
+    } elseif ($current_day > $day_of_meeting) {
+	$days_until_meeting = 7 - ($current_day - $day_of_meeting);
+    } else {
+	$days_until_meeting = $day_of_meeting - $current_day;
+    }
+
+    $today = mktime($hour_of_meeting, $minutes_of_meeting, 0, $current_month,
+		    $current_date, $current_year);
+
+    $meeting_time = $today + ($length_of_day * $days_until_meeting);
+
+    return $meeting_time;
+}
+
+#This function takes a unix datetime and converts it to a nice friendly string
+#depending on what day today is.  So, if the datetime won't happen for the
+#a while it returns a string like, "Tuesday, March 16th @ 8:00pm", but if the
+#meeting is tomorrow it will return, "Tomorrow @ 8:00pm" and if the meeting is
+#tonight it will return, "Tonight @ 8:00pm".
+function get_nice_text_for_time($meeting_time) {
+    $today = getdate();
+    $meeting = getdate($meeting_time);
+  
+    $text = "";
+    if (($today["year"] == $meeting["year"]) and
+	($today["month"] == $meeting["month"])) {
+
+	if ($today["mday"] == $meeting["mday"]) {
+	    if ($meeting["hours"] >= 18) {
+		$text .= "Tonight";
+	    } else {
+		$text .= "Today";
+	    }
+	} elseif (($today["mday"] + 1) == $meeting["mday"]) {
+	    $text .= "Tomorrow";
+	}
+    }
+
+    if ($text == "") {
+	$text .= date("l, F jS", $meeting_time);
+    }
+
+    $text .= " @ " . date("g:ia", $meeting_time);
+
+    return $text;
+}
+
+?>
